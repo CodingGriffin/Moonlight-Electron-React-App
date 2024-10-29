b4a72d7bde28195f58e564e7a1eec8b4957bfb0d
CodingGriffin <biggriffin640@gmail.com>
2024-10-29 21:04:01 +0100

    add sheet page

---
+++ src/renderer/component/Sheet.tsx
@@ -0,0 +1,19 @@
+import expand from '../../../assets/images/sheet/expand.svg';
+import bin from '../../../assets/images/sheet/bin.svg';
+
+function SheetComponent() {
+  return (
+    <div className="w-56 h-72 bg-white text-black">
+      <div className="text-center text-xl p-10 h-48">Roofer in Ireland</div>
+      <div className="text-center h-12 p-3 border-t border-grey">
+        Oct 14, 2014
+      </div>
+      <div className="flex flex-row justify-center">
+        <img className="mr-2" src={expand} alt="expand" />
+        <img className="ml-2" src={bin} alt="bin" />
+      </div>
+    </div>
+  );
+}
+
+export default SheetComponent;
--- src/renderer/page/Sheet/Sheet.tsx
+++ src/renderer/page/Sheet/Sheet.tsx
@@ -1,7 +1,23 @@
+import SheetComponent from '../../component/Sheet';
+import './style.css';
+
 function Sheet() {
   return (
-    <div className="w-full">
-      <div>Sheet</div>
+    <div className="flex flex-col rounded-3xl mx-8 p-10 sheet-bg">
+      <div className="flex flex-row justify-between my-5">
+        <SheetComponent />
+        <SheetComponent />
+        <SheetComponent />
+        <SheetComponent />
+        <SheetComponent />
+      </div>
+      <div className="flex flex-row justify-between my-5">
+        <SheetComponent />
+        <SheetComponent />
+        <SheetComponent />
+        <SheetComponent />
+        <SheetComponent />
+      </div>
     </div>
   );
 }
