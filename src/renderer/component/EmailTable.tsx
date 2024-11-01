bcf9c037cc713fc210a0194e0a7665109200d66f
CodingGriffin <biggriffin640@gmail.com>
2024-11-01 01:54:24 +0100

    add email

---
+++ src/renderer/component/EmailTable.tsx
@@ -0,0 +1,136 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+interface Result {
+  to: string;
+  subject: string;
+}
+
+interface TabComponentProps {
+  data: Result[];
+}
+
+function EmailTable({ data }: TabComponentProps) {
+  return (
+    <div className="relative h-auto">
+      <table className="w-full text-sm text-center text-gray-500">
+        <thead className="text-sm">
+          <tr className="">
+            <th scope="col" className="pl-5">
+              <div className="flex items-center">
+                <input
+                  id="checkbox-all"
+                  type="checkbox"
+                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label htmlFor="checkbox-table-1" className="sr-only">
+                  checkbox
+                </label>
+              </div>
+            </th>
+            <th scope="col" className="">
+              No
+            </th>
+            <th scope="col" className="">
+              To
+            </th>
+            <th scope="col" className="">
+              Subjescr
+            </th>
+          </tr>
+        </thead>
+        <tbody className="text-white">
+          {data && data.length ? (
+            data.map((_item: Result, index) => {
+              return (
+                <tr className="border-t border-gray-500 border-solid">
+                  <td className="w-4 p-4 pl-5">
+                    <div className="flex items-center">
+                      <input
+                        id="checkbox-table-1"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      />
+                      <label htmlFor="checkbox-table-1" className="sr-only">
+                        checkbox
+                      </label>
+                    </div>
+                  </td>
+                  <th className="px-2 py-5">{index + 1}</th>
+                  <td className="px-2">{_item.to}</td>
+                  <td className="px-2">{_item.subject}</td>
+                </tr>
+              );
+            })
+          ) : (
+            <div>No Data To Show</div>
+          )}
+        </tbody>
+      </table>
+      <nav
+        className="flex items-center flex-column flex-wrap md:flex-row justify-center pt-4 pb-b"
+        aria-label="Table navigation"
+      >
+        <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 rounded-s-lg hover:text-white"
+            >
+              Previous
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-white"
+            >
+              1
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-white"
+            >
+              2
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              aria-current="page"
+              className="flex items-center justify-center px-3 h-8 text-white bg-stone-100 hover:text-white"
+            >
+              3
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-white"
+            >
+              4
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 hover:text-white"
+            >
+              5
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 rounded-e-lg hover:text-white"
+            >
+              Next
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default EmailTable;
--- src/renderer/page/Email/Email.tsx
+++ src/renderer/page/Email/Email.tsx
@@ -1,7 +1,46 @@
+import EmailTable from '../../component/EmailTable';
+import binSvg from '../../../../assets/images/email/bin.svg';
+import pencilSvg from '../../../../assets/images/email/pencil.svg';
+import './style.css';
+
+const mockupData = [
+  {
+    to: 'info@irishroofer.ie',
+    subject: 'fix our roofers',
+  },
+  {
+    to: 'info@irishroofer.ie',
+    subject: 'fix our roofers',
+  },
+  {
+    to: 'info@irishroofer.ie',
+    subject: 'fix our roofers',
+  },
+];
+
 function Email() {
   return (
-    <div className="w-full">
-      <div>Email</div>
+    <div className="rounded-3xl pb-10 email-bg mx-10">
+      <div className="py-10 px-8 flex justify-between">
+        <h2 className="flex">Inbox</h2>
+        <div className="flex">
+          <button
+            className="flex flex-row items-center mr-5 h-10 button-delete"
+            type="button"
+          >
+            <img className="mr-5" src={binSvg} alt="delete" />
+            <span className="text-md">Delete</span>
+          </button>
+          <button
+            className="flex flex-row items-center ml-5 h-10 button-compose"
+            type="button"
+          >
+            <img className="mr-5" src={pencilSvg} alt="compose" />
+            <span className="text-black">Compose</span>
+          </button>
+        </div>
+      </div>
+      <EmailTable data={mockupData} />
     </div>
   );
 }
