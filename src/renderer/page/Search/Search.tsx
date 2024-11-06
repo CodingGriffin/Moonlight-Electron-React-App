acd8ecd01e028547571cb35e756b6a5620d50a2b
CodingGriffin <biggriffin640@gmail.com>
2024-11-06 03:31:51 +0100

    modify google map search to by point & radius

--- src/renderer/page/Search/Search.tsx
+++ src/renderer/page/Search/Search.tsx
@@ -1,5 +1,11 @@
 /* eslint-disable no-undef */
-import React, { useCallback, useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useState,
+  MouseEvent,
+  useRef,
+} from 'react';
 import Map from '../../component/Map';
 import TabComponent from '../../component/Tab';
 import './style.css';
@@ -7,6 +13,9 @@ import './style.css';
 interface SearchData {
   query: string;
   num: number;
+  radius: number;
+  lat: number;
+  lng: number;
 }
 
 interface SearchProps {
@@ -15,7 +24,19 @@ interface SearchProps {
   exportResult: (data: any) => Promise<string>;
 }
 
+interface Coordinate {
+  lat: number;
+  lng: number;
+}
+
 function Search({ result, getResult, exportResult }: SearchProps) {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState<{
+    x: number;
+    y: number;
+  }>({ x: 0, y: 0 });
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [coordinate, setCoordinate] = useState<Coordinate>();
   const [range, setRangeValue] = useState(50);
   const [query, setQuery] = useState('');
   const [count, setCount] = useState(10);
@@ -23,6 +44,16 @@ function Search({ result, getResult, exportResult }: SearchProps) {
     useState<google.maps.LatLng | null>(null);
   const [currentLocation, setCurrentLocation] =
     useState<google.maps.LatLng | null>(null);
+
+  const handleMouseMove = (event: MouseEvent<HTMLInputElement>) => {
+    if (inputRef.current) {
+      const rect = inputRef.current.getBoundingClientRect();
+      const x = event.pageX - rect.left; // Get x position relative to the viewport
+      const y = 0; // Get y position relative to the viewport
+
+      setTooltipPosition({ x: x - 25, y: y + 20 }); // Offset for visibility
+    }
+  };
 
   useEffect(() => {
     // Get user's current location
@@ -47,6 +78,7 @@ function Search({ result, getResult, exportResult }: SearchProps) {
     if (latLng) {
       const lat = latLng.lat();
       const lng = latLng.lng();
+      setCoordinate({ lat, lng });
       setMarkerPosition(new google.maps.LatLng(lat, lng));
       setCurrentLocation(new google.maps.LatLng(lat, lng));
     }
@@ -68,6 +100,9 @@ function Search({ result, getResult, exportResult }: SearchProps) {
     const searchData: SearchData = {
       query,
       num: count,
+      radius: range,
+      lat: coordinate?.lat || -34,
+      lng: coordinate?.lng || 150,
     };
 
     if (typeof getResult === 'function') {
@@ -85,14 +120,39 @@ function Search({ result, getResult, exportResult }: SearchProps) {
           markerPosition={markerPosition || null}
           onMapClick={onMapClick}
         />
-        <input
-          type="range"
-          min={0}
-          max="200"
-          value={range}
-          onChange={handleRangeChange}
-          className="range range-primary w-full mt-4"
-        />
+        <div className="input-container w-full">
+          <p className="mt-3">Range</p>
+          <input
+            type="range"
+            min={0}
+            max="200"
+            value={range}
+            onChange={handleRangeChange}
+            onMouseEnter={() => setTooltipVisible(true)}
+            // onMouseLeave={() => setTooltipVisible(false)}
+            onMouseMove={handleMouseMove}
+            className="range range-primary w-full mt-4"
+            ref={inputRef} // Attach ref to the input element
+          />
+          {tooltipVisible && (
+            <div
+              className="tooltip"
+              style={{
+                position: 'absolute',
+                left: tooltipPosition.x,
+                top: tooltipPosition.y,
+                backgroundColor: '#333',
+                color: '#fff',
+                padding: '5px',
+                borderRadius: '5px',
+                pointerEvents: 'none', // Prevents mouse events on the tooltip
+                zIndex: 10,
+              }}
+            >
+              {range}km
+            </div>
+          )}
+        </div>
         <div className="flex-col my-5 p-5 rounded-3xl search-limits">
           <p className="mb-3">Scraping Limits</p>
           <input
