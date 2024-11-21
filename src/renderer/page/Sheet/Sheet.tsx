b26e6d4fb497128f4bfc36e5ed2989a6042a5f6b
CodingGriffin <biggriffin640@gmail.com>
2024-11-21 09:25:44 +0100

    add drag and drop template for sheet

--- src/renderer/page/Sheet/Sheet.tsx
+++ src/renderer/page/Sheet/Sheet.tsx
@@ -1,3 +1,4 @@
+import { useDragAndDrop } from '@formkit/drag-and-drop/react';
 import SheetComponent from '../../component/Sheet';
 import './style.css';
 
@@ -6,8 +7,24 @@ interface SheetProps {
 }
 
 function Sheet({ result }: SheetProps) {
+  const [parent, tapes] = useDragAndDrop<HTMLUListElement, string>([
+    'Depeche Mode',
+    'Duran Duran',
+    'Pet Shop Boys',
+    'Kraftwerk',
+    'Tears for Fears',
+    'Spandau Ballet',
+  ]);
+
   return (
     <div className="flex flex-col rounded-3xl mx-8 p-10 sheet-bg">
+      {/* <ul ref={parent}>
+        {tapes.map((tape) => (
+          <li className="cassette" data-label={tape} key={tape}>
+            {tape}
+          </li>
+        ))}
+      </ul> */}
       <div className="flex flex-row justify-between my-5">
         {result?.map((item: any) => {
           return <SheetComponent sheet={item} />;
