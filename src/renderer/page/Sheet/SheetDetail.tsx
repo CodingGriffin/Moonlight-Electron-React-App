765bca8cc21c3888b2edcecaacdf45c88e4d6049
CodingGriffin <biggriffin640@gmail.com>
2024-11-25 00:26:55 -0400

    fix the sheet detail page so that support the responsive

--- src/renderer/page/Sheet/SheetDetail.tsx
+++ src/renderer/page/Sheet/SheetDetail.tsx
@@ -1,3 +1,4 @@
+import Header from '../../component/Header';
 import ResultTable from '../../component/ResultTable';
 // import filterSvg from '../../../../assets/images/button_icon/filter.svg';
 // import downloadSvg from '../../../../assets/images/button_icon/download.svg';
@@ -28,41 +29,44 @@ function SheetDetail({ result, favorite }: HistoryProps) {
   // };
 
   return (
-    <div className="rounded-3xl pb-10 my-10">
-      {/* <div className="py-10 px-8 flex justify-between">
-        <h2 className="flex">Search Results</h2>
-        <div className="flex">
-          <button
-            className="flex flex-row items-center mr-5 h-10 button-filter"
-            type="button"
-          >
-            <img className="mr-5" src={filterSvg} alt="filter" />
-            <span className="text-md">Filter</span>
-          </button>
-          <button
-            className="flex flex-row items-center mx-5 h-10 button-download"
-            type="button"
-            onClick={downloadResult}
-          >
-            <img className="mr-5" src={downloadSvg} alt="export" />
-            <span className="text-black">Download</span>
-          </button>
-          <button
-            className="flex flex-row items-center ml-5 h-10 button-export"
-            type="button"
-            onClick={handleExportButton}
-          >
-            <img className="mr-5" src={downloadSvg} alt="export" />
-            <span className="text-black">Export</span>
-          </button>
-        </div>
-      </div> */}
-      <ResultTable
-        data={result.map((item: any[]) => {
-          return item[0];
-        })}
-        favorite={favorite}
-      />
+    <div>
+      <Header title="sheet detail" />
+      <div className="rounded-3xl pb-10 sm:my-2 mx-2 lg:my-10 mx-5">
+        {/* <div className="py-10 px-8 flex justify-between">
+          <h2 className="flex">Search Results</h2>
+          <div className="flex">
+            <button
+              className="flex flex-row items-center mr-5 h-10 button-filter"
+              type="button"
+            >
+              <img className="mr-5" src={filterSvg} alt="filter" />
+              <span className="text-md">Filter</span>
+            </button>
+            <button
+              className="flex flex-row items-center mx-5 h-10 button-download"
+              type="button"
+              onClick={downloadResult}
+            >
+              <img className="mr-5" src={downloadSvg} alt="export" />
+              <span className="text-black">Download</span>
+            </button>
+            <button
+              className="flex flex-row items-center ml-5 h-10 button-export"
+              type="button"
+              onClick={handleExportButton}
+            >
+              <img className="mr-5" src={downloadSvg} alt="export" />
+              <span className="text-black">Export</span>
+            </button>
+          </div>
+        </div> */}
+        <ResultTable
+          data={result.map((item: any[]) => {
+            return item[0];
+          })}
+          favorite={favorite}
+        />
+      </div>
     </div>
   );
 }
